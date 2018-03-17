c799faaef3c278ebf330716dbec3a41a84451215
Robert Wells <robert.wells@thatgamesguy.co.uk>
2018-03-17 09:15:44 +0000

    renamed build folder

---
+++ XCode Build/that genetic algorithm/SceneGame.cpp
@@ -0,0 +1,48 @@
+#include "SceneGame.hpp"
+
+SceneGame::SceneGame(WorkingDirectory& workingDir) : workingDir(workingDir)
+{
+    
+}
+
+void SceneGame::OnCreate()
+{
+    std::shared_ptr<Object> player = std::make_shared<Object>();
+    
+    auto sprite = player->AddComponent<C_Sprite>();
+    sprite->SetTextureAllocator(&textureAllocator);
+    sprite->Load(workingDir.Get() + "viking.png");
+    
+    auto movement = player->AddComponent<C_KeyboardMovement>();
+    movement->SetInput(&input);
+    
+    objects.Add(player);
+}
+
+void SceneGame::OnDestroy()
+{
+    
+}
+
+void SceneGame::ProcessInput()
+{
+    input.Update();
+}
+
+void SceneGame::Update(float deltaTime)
+{
+    objects.ProcessRemovals();
+    objects.ProcessNewObjects();
+    
+    objects.Update(deltaTime);
+}
+
+void SceneGame::LateUpdate(float deltaTime)
+{
+    objects.LateUpdate(deltaTime);
+}
+
+void SceneGame::Draw(Window& window)
+{
+    objects.Draw(window);
+}
