468eb21eeb473641df40ee93d3f14f749a13c814
Robert Wells <robert.wells@thatgamesguy.co.uk>
2018-03-17 09:12:09 +0000

    moved source from that game engine

---
+++ that genetic algorithm/that genetic algorithm/SceneGame.hpp
@@ -0,0 +1,33 @@
+#ifndef SceneGame_hpp
+#define SceneGame_hpp
+
+#include "Scene.hpp"
+#include "Input.hpp"
+#include "WorkingDirectory.hpp"
+#include "Object.hpp"
+#include "C_Sprite.hpp"
+#include "C_KeyboardMovement.hpp"
+#include "ResourceAllocator.hpp"
+#include "ObjectCollection.hpp"
+
+class SceneGame : public Scene
+{
+public:
+    SceneGame(WorkingDirectory& workingDir);
+    
+    void OnCreate() override;
+    void OnDestroy() override;
+    
+    void ProcessInput() override;
+    void Update(float deltaTime) override;
+    void LateUpdate(float deltaTime) override;
+    void Draw(Window& window) override;
+    
+private:
+    WorkingDirectory& workingDir;
+    Input input;
+    ResourceAllocator<sf::Texture> textureAllocator;
+    ObjectCollection objects;
+};
+
+#endif /* SceneGame_hpp */
